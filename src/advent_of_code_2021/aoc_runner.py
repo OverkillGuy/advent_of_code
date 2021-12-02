29ea96aade05d88dbae9e7f1096485d4b0e164ba
Jb DOYON <jb@jiby.tech>
2021-12-02 00:05:42 +0000

    Add day 1 solution 2

--- src/advent_of_code_2021/aoc_runner.py
+++ src/advent_of_code_2021/aoc_runner.py
@@ -8,7 +8,7 @@ from advent_of_code_2021.days import day0, day1
 DAYS = {
     # Dummy day0 with real function to run tests
     0: {1: day0.solution1, 2: day0.solution2},
-    1: {1: day1.solution1},
+    1: {1: day1.solution1, 2: day1.solution2},
     # Fake day29 with just no solution2
     29: {1: lambda x: True},
 }
--- src/advent_of_code_2021/days/day1.py
+++ src/advent_of_code_2021/days/day1.py
@@ -10,3 +10,17 @@ def solution1(input_list):
     off_by_one_list = input_list[1:]
     deltas = [i1 - i0 for i0, i1 in zip(input_list, off_by_one_list)]
     return sum([1 if delta > 0 else 0 for delta in deltas])
+
+
+def solution2(input_list):
+    """Solve day1 solution2 via zips
+
+    >>> solution2([607, 618, 618, 617, 647, 716, 769, 792])
+    5
+    """
+    off_by_one_list = input_list[1:]
+    off_by_two_list = input_list[2:]
+    totals = [
+        i0 + i1 + i2 for i0, i1, i2 in zip(input_list, off_by_one_list, off_by_two_list)
+    ]
+    return solution1(totals)
