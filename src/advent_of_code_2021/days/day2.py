e8cbaea84273387c8469e5ee398ca079ec840795
Jb DOYON <jb@jiby.tech>
2021-12-02 17:28:41 +0000

    Day 2 RED test

--- src/advent_of_code_2021/days/day2.py
+++ src/advent_of_code_2021/days/day2.py
@@ -1,7 +1,7 @@
 """Day 2 solution using Complex numbers for 2D vectors"""
 
 from enum import Enum
-from typing import Dict, List
+from typing import Dict, List, NamedTuple
 
 
 class Direction(str, Enum):
@@ -41,3 +41,30 @@ def solution1(input_list: List[str]) -> int:
     """
     position = follow_directions(input_list)
     return int(position.real * position.imag)
+
+
+class Pose(NamedTuple):
+    """Aggregate of position, depth and aim"""
+
+    position: int
+    depth: int
+    aim: int
+
+
+def apply_movement_aim(before: Pose, movement: str) -> Pose:
+    """Apply a single movement to a Pose
+
+    >>> apply_movement_aim(Pose(position=0, depth=0, aim=0), "forward 5")
+    Pose(position=5, depth=0, aim=0)
+    >>> apply_movement_aim(Pose(position=5, depth=0, aim=0), "down 5")
+    Pose(position=5, depth=0, aim=5)
+    >>> apply_movement_aim(Pose(position=5, depth=0, aim=5), "forward 8")
+    Pose(position=13, depth=40, aim=5)
+    >>> apply_movement_aim(Pose(position=13, depth=40, aim=5), "up 3")
+    Pose(position=13, depth=40, aim=2)
+    >>> apply_movement_aim(Pose(position=13, depth=40, aim=2), "down 8")
+    Pose(position=13, depth=40, aim=10)
+    >>> apply_movement_aim(Pose(position=13, depth=40, aim=10), "forward 2")
+    Pose(position=15, depth=60, aim=10)
+    """
+    return Pose(0, 0, 0)
