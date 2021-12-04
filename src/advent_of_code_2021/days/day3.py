e3044b46d9da18fa07a6a8c06257c4dd33efa7f2
Jb DOYON <jb@jiby.tech>
2021-12-04 00:01:33 +0000

    Add day3 solution 1

---
+++ src/advent_of_code_2021/days/day3.py
@@ -0,0 +1,51 @@
+"""Day 3, binary things"""
+
+from typing import List
+
+
+def digit_list_to_int(digits: List[int]) -> int:
+    """Transform a list of binary digits into the base 10 integer it represents
+
+    >>> digit_list_to_int([1, 0, 1, 1, 0])
+    22
+    >>> digit_list_to_int([0, 1, 0, 0, 1])
+    9
+    """
+    digits_as_string = "".join([str(i) for i in digits])
+    return int(digits_as_string, 2)
+
+
+def count_one_digits(input_list: List[str]) -> List[int]:
+    """Count the number of digits set to one per number
+
+    >>> count_one_digits(['011', '101', '111'])
+    [2, 2, 3]
+    >>> count_one_digits(["011110111110", "110111000111"])
+    [1, 2, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1]
+    """
+    numbers_list = [number_str.strip() for number_str in input_list]
+    digit_one_counts = [0] * len(numbers_list[0])
+    for number in numbers_list:
+        for digit_index, digit in enumerate(list(number)):
+            digit_one_counts[digit_index] += int(digit)
+    return digit_one_counts
+
+
+def solution1(input_list: List[str]) -> int:
+    """Solve day3 problem 1
+
+    >>> solution1(["00100", "11110", "10110", "10111", "10101", "01111", "00111",
+    ...            "11100", "10000", "11001", "00010", "01010"])
+    198
+    """
+    digit_one_counts = count_one_digits(input_list)
+    half_of_input = len(input_list) // 2
+    gamma_digits = [
+        1 if digit_one_count > half_of_input else 0
+        for digit_one_count in digit_one_counts
+    ]
+    epsilon_digits = [
+        1 if digit_one_count == 0 else 0 for digit_one_count in gamma_digits
+    ]
+    gamma, epsilon = digit_list_to_int(gamma_digits), digit_list_to_int(epsilon_digits)
+    return gamma * epsilon
--- src/advent_of_code_2021/solutions_lookup.py
+++ src/advent_of_code_2021/solutions_lookup.py
@@ -2,13 +2,14 @@
 
 from typing import Callable, Dict
 
-from advent_of_code_2021.days import day0, day1, day2
+from advent_of_code_2021.days import day0, day1, day2, day3
 
 SOLUTION_LOOKUP_DAYS: Dict[int, Dict[int, Callable]] = {
-    # Dummy day0 with real function to run tests
+    # Dummy day0 with real function to run OK tests
     0: {1: day0.solution1, 2: day0.solution2},
     1: {1: day1.solve1_stringlist, 2: day1.solve2_stringlist},
     2: {1: day2.solution1, 2: day2.solution2},
-    # Fake day29 with just no solution2
+    3: {1: day3.solution1},
+    # Fake day29 with just solution 1, no solution2, for testing
     29: {1: lambda x: True},
 }
