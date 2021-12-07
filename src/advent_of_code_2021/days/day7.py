532713e3fb1e285f6ed04732fe8d5edec6289390
Jb DOYON <jb@jiby.tech>
2021-12-07 18:10:02 +0000

    Add day7 part 2

--- src/advent_of_code_2021/days/day7.py
+++ src/advent_of_code_2021/days/day7.py
@@ -44,3 +44,52 @@ def align_crabs(crabs: List[int]) -> int:
 def solution1(input_str: str) -> int:
     """Solve day 7 part 1"""
     return align_crabs(csv_to_integer_list(input_str))
+
+
+def cost_alignment2(crabs: List[int], target_position: int) -> int:
+    """Calculate the alignment cost of a position for part 2
+
+    >>> cost_alignment2(sample_crabs, 5)
+    168
+    >>> cost_alignment2(sample_crabs, 2)
+    206
+    """
+    return sum(sum_till_n(abs(position - target_position)) for position in crabs)
+
+
+def sum_till_n(n: int) -> int:
+    """Calculate the sum of numbers from 1 to n included
+
+    >>> sum_till_n(1)
+    1
+    >>> sum_till_n(2)
+    3
+    >>> sum_till_n(5)
+    15
+    """
+    return (n * (n + 1)) // 2
+
+
+def align_crabs2(crabs: List[int]) -> int:
+    """Find best alignment for crabs using second part calculation
+
+    >>> align_crabs2(sample_crabs)
+    168
+    """
+    crab_position_bins = Counter(crabs)
+    max_position = max(crab_position_bins.keys())
+    # Upper boundary of sum_till_n cost: x ^ 2 > (x * (x+1)) / 2 > (x ** 2 ) / 2
+    # Heuristic for max cost: for each crab, take upper boundary of dist
+    best_cost: int = (max_position ** 2) * len(crabs)
+    # best_position: int = -1
+    for position in range(max_position + 1):
+        cost = cost_alignment2(crabs, position)
+        if cost < best_cost:
+            # best_position = position
+            best_cost = cost
+    return best_cost
+
+
+def solution2(input_str: str) -> int:
+    """Solve day 7 part 2"""
+    return align_crabs2(csv_to_integer_list(input_str))
