7387dda05407080d2710ab1177d3b9b859b80233
Jb DOYON <jb@jiby.tech>
2022-12-04 21:16:44 +0000

    Solve day 3 part 1

--- src/advent_of_code/aoc2022/day3.py
+++ src/advent_of_code/aoc2022/day3.py
@@ -1,5 +1,8 @@
 """Day 3 solution to AoC 2022"""
 
+import string
+from functools import cache
+
 Compartment = str
 """The content of a single rucksack compartment, as a string"""
 Rucksack = tuple[Compartment, Compartment]
@@ -23,9 +26,58 @@ SAMPLE_INPUT: list[Rucksack] = [
 ]
 
 
-def solution3(puzzle_input) -> int:
-    """Solve day3 part 1"""
-    return 0
+def only_common_item(sack: Rucksack) -> str:
+    """Return the only item in sack common between compartments
+
+    >>> only_common_item(("vJrwpWtwJgWr", "hcsFMMfFFhFp"))
+    'p'
+    >>> only_common_item(("jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"))
+    'L'
+    >>> only_common_item(("PmmdzqPrV", "vPwwTWBwg"))
+    'P'
+    """
+    first, second = sack
+    set_intersection = set(list(first)) & set(list(second))
+    # Problem definition guarantees only one char intersection
+    return set_intersection.pop()
+
+
+@cache
+def char_priority(char: str) -> int:
+    """Compute the priority of a character
+
+    >>> char_priority("a"), char_priority("b"), char_priority("z")
+    (1, 2, 26)
+    >>> char_priority("A"), char_priority("B"), char_priority("Z")
+    (27, 28, 52)
+    """
+    if char not in string.ascii_letters:
+        raise ValueError(f"Character priority only works on chars; Was given '{char}'")
+    ascii_value_lower_a = ord("a")
+    PRIORITY_LOOKUP: dict[str, int] = {}
+    for char_lower in string.ascii_lowercase:
+        ascii_value_char = ord(char_lower)
+        ascii_offset = ascii_value_char - ascii_value_lower_a
+        PRIORITY_LOOKUP[char_lower] = ascii_offset + 1
+    ascii_value_upper_a = ord("A")
+    for char_upper in string.ascii_uppercase:
+        ascii_value_char = ord(char_upper)
+        ascii_offset = ascii_value_char - ascii_value_upper_a
+        PRIORITY_LOOKUP[char_upper] = ascii_offset + 26 + 1
+    return PRIORITY_LOOKUP[char]
+
+
+def solution1(puzzle_input: list[Rucksack]) -> int:
+    """Solve day3 part 1
+
+    >>> solution1(SAMPLE_INPUT)
+    157
+    """
+    acc = 0
+    for rucksack in puzzle_input:
+        intersection_char = only_common_item(rucksack)
+        acc += char_priority(intersection_char)
+    return acc
 
 
 def solution2(puzzle_input) -> int:
@@ -50,7 +102,7 @@ def read_puzzle_input(puzzle_input: str) -> list[Rucksack]:
 
 def solve1_string(puzzle_input: str) -> int:
     """Convert list to proper format and solve day3 solution1"""
-    return solution2(read_puzzle_input(puzzle_input))
+    return solution1(read_puzzle_input(puzzle_input))
 
 
 def solve2_string(puzzle_input: str) -> int:
