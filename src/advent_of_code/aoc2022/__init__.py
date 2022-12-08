a71084eee1ffd6d03a419397a46f512e32615ed5
Jb DOYON <jb@jiby.tech>
2022-12-08 00:44:10 +0000

    Add input parsing for day 7

--- src/advent_of_code/aoc2022/__init__.py
+++ src/advent_of_code/aoc2022/__init__.py
@@ -3,7 +3,7 @@
 from typing import Callable
 
 from advent_of_code import Day, Part
-from advent_of_code.aoc2022 import day1, day2, day3, day4, day5, day6
+from advent_of_code.aoc2022 import day1, day2, day3, day4, day5, day6, day7
 
 SOLUTIONS_2022: dict[Day, dict[Part, Callable]] = {
     1: {1: day1.solve1_string, 2: day1.solve2_string},
@@ -12,4 +12,5 @@ SOLUTIONS_2022: dict[Day, dict[Part, Callable]] = {
     4: {1: day4.solve1_string, 2: day4.solve2_string},
     5: {1: day5.solve1_string, 2: day5.solve2_string},
     6: {1: day6.solution1, 2: day6.solution2},
+    7: {1: day7.solve1_string, 2: day7.solve2_string},
 }
