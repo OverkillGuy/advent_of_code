3e11e66831510daaa130fc61a7d3407e904a7d28
Jb DOYON <jb@jiby.tech>
2023-12-01 23:16:44 +0000

    Solve day 1 part 2

--- src/advent_of_code/aoc2023/day1.py
+++ src/advent_of_code/aoc2023/day1.py
@@ -1,7 +1,7 @@
 """Day 1 solution to AoC 2023"""
 
+
 from advent_of_code.input_conversion import to_string_list
-from copy import deepcopy
 
 SAMPLE_INPUT = [
     "1abc2",
@@ -19,19 +19,27 @@ SAMPLE_INPUT2 = [
     "zoneight234",
     "7pqrstsixteen",
 ]
-CALIBRATION2 = [29, 83, 13,24,42,14,76]
+CALIBRATION2 = [29, 83, 13, 24, 42, 14, 76]
 
 
-digits = {"one": "1",
-          "two": "2",
-          "three": "3",
-          "four": "4",
-          "five": "5",
-          "six": "6",
-          "seven": "7",
-          "eight": "8",
-          "nine":"9",
+digits = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
+
+
+def first_last(line: str):
+    """Grab first and last digit of each"""
+    digits = [s for s in line if s.isdigit()]
+    first, last = digits[0], digits[-1]
+    return int(first + last)
 
 
 def solution1(puzzle_input: list[str]):
@@ -42,36 +50,33 @@ def solution1(puzzle_input: list[str]):
     """
     return sum(first_last(line) for line in puzzle_input)
 
-def first_last(line: str):
-    """Grab first and last digit of each"""
-    digits = [s for s in line if s.isdigit()]
-    first, last = digits[0], digits[-1]
-    return int(first + last)
 
+def filter_digits(s: str):
+    """Replace and filter digits, even if in string form (part 2)
 
-def replace(s: str):
-    """Replace digits in string form, once"""
-    acc = deepcopy(s)
-    digits_loc = {acc.find(d):d for d in digits.keys() if acc.find(d) >= 0}
-    if not digits_loc:
-        return s
-    replace_index = min(digits_loc.keys())
-    replace_val = digits_loc[replace_index]
-    acc = acc.replace(replace_val, digits[replace_val])
-    return acc
+    Note the crucial check here: overlaps are allowed:
 
-def recurse_replace(s: str):
-    """Replace digits recursively until no more to replace
+    >>> filter_digits("eighthree")
+    83
+    >>> filter_digits("sevenine")
+    79
 
-    >>> all([first_last(recurse_replace(i)) == s for i,s in zip(SAMPLE_INPUT2, CALIBRATION2)])
+    And cross-checking the given solutions:
+
+    >>> all([filter_digits(i) == s for i,s in zip(SAMPLE_INPUT2, CALIBRATION2)])
     True
     """
-    prev = ""
-    acc = deepcopy(s)
-    while acc != prev:
-        prev = acc
-        acc = replace(acc)
-    return acc
+    acc = []
+    for index, char in enumerate(s):
+        if char.isdigit():
+            acc.append(char)
+        for digit_s, digit_i in digits.items():
+            # This char spells a digit: add it too (but no modifying s!)
+            if s[index:].find(digit_s) == 0:
+                acc.append(digit_i)
+    first, last = acc[0], acc[-1]
+    return int(first + last)
+
 
 def solution2(puzzle_input: list[str]):
     """Solve day1 solution2
@@ -79,12 +84,13 @@ def solution2(puzzle_input: list[str]):
     >>> solution2(SAMPLE_INPUT2)
     281
     """
-    return solution1([recurse_replace(line) for line in puzzle_input])
+    return sum(filter_digits(line) for line in puzzle_input)
 
 
 def solve1_string(puzzle_input: str) -> int:
     """Convert list to proper format and solve day1 solution1"""
     return solution1(to_string_list(puzzle_input))
+
 
 def solve2_string(puzzle_input: str) -> int:
     """Convert list to proper format and solve day1 solution2"""
