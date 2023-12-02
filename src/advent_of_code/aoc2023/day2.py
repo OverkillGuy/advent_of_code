2be10ae4929481ba1e9205d9d0c917d2e9453dd0
Jb DOYON <jb@jiby.tech>
2023-12-02 16:09:53 +0000

    Add day2 input parsing

--- src/advent_of_code/aoc2023/day2.py
+++ src/advent_of_code/aoc2023/day2.py
@@ -1,5 +1,8 @@
 """Day 2 solution to AoC 2023"""
+import re
 from typing import Literal, TypeAlias
+
+from advent_of_code.input_conversion import to_string_list
 
 """First, we state the Puzzle Input objects, as given.
 Note these may be terrible to use for solving, but perfect to represent the inputs."""
@@ -50,6 +53,15 @@ SAMPLE_INPUT: Games = [
 ]
 
 
+GAMES_SPLIT_REGEX = re.compile("^Game (\d+): (.+)$")
+"""Splits game into id + grabs"""
+
+COLORS_REGEX = "|".join(Color.__args__)  # type: ignore[attr-defined]
+"""The Color literal, as regex for {py:const}`GRAB_SPLIT_REGEX`"""
+GRAB_SPLIT_REGEX = re.compile(f"(\d+) ({COLORS_REGEX})")
+"""How to split a single grab into color pairs"""
+
+
 def solution1(puzzle_input) -> int:
     """Solve day2 part 1"""
     return 0
@@ -60,9 +72,28 @@ def solution2(puzzle_input) -> int:
     return 0
 
 
-def read_puzzle_input(puzzle_input: str):
-    """Process the puzzle input string"""
-    return puzzle_input
+def read_puzzle_input(puzzle_input: str) -> Games:
+    """Process the puzzle input string
+
+    >>> read_puzzle_input(SAMPLE_INPUT_STR) == SAMPLE_INPUT
+    True
+    """
+    acc: Games = []
+    for line in to_string_list(puzzle_input):
+        matches = GAMES_SPLIT_REGEX.match(line)
+        if matches is None:
+            raise ValueError(f"Bad game line: '{line}'")
+        game_id, game_str = matches.groups()
+        grab_lst = [grab.strip() for grab in game_str.split(";")]
+        grabs: list[Grab] = []
+        for grab in grab_lst:
+            grab_matches = GRAB_SPLIT_REGEX.findall(grab)
+            grab_obj: Grab = {}
+            for num, color in grab_matches:
+                grab_obj[color] = int(num)
+            grabs.append(grab_obj)
+        acc.append((int(game_id), grabs))
+    return acc
 
 
 def solve1_string(puzzle_input: str) -> int:
