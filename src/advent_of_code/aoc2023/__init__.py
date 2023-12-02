ac766ea080e1b252c1240a5a99b5f98d644d3609
Jb DOYON <jb@jiby.tech>
2023-12-02 15:08:31 +0000

    Add day2 stub

--- src/advent_of_code/aoc2023/__init__.py
+++ src/advent_of_code/aoc2023/__init__.py
@@ -3,8 +3,9 @@
 from typing import Callable
 
 from advent_of_code import Day, Part
-from advent_of_code.aoc2023 import day1
+from advent_of_code.aoc2023 import day1, day2
 
 SOLUTIONS_2023: dict[Day, dict[Part, Callable]] = {
     1: {1: day1.solve1_string, 2: day1.solve2_string},
+    2: {1: day2.solve1_string},  # , 2: day2.solve2_string
 }
