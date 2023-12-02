cdf724fb9516daf16bc0ffdfc17e8d0560115729
Jb DOYON <jb@jiby.tech>
2023-12-02 15:23:35 +0000

    Add day2 input types

--- src/advent_of_code/aoc2023/day2.py
+++ src/advent_of_code/aoc2023/day2.py
@@ -1,4 +1,18 @@
 """Day 2 solution to AoC 2023"""
+from typing import Literal, TypeAlias
+
+"""First, we state the Puzzle Input objects, as given.
+Note these may be terrible to use for solving, but perfect to represent the inputs."""
+GameID: TypeAlias = int
+"""A single game's ID"""
+Color: TypeAlias = Literal["red", "blue", "green"]
+"""The potential colors of cubs"""
+Grab: TypeAlias = dict[Color, int]
+"""A single grab in the bag = how many of each color was revealed"""
+Game: TypeAlias = tuple[GameID, list[Grab]]
+"""A single game = sequence of multiple grabs"""
+Games: TypeAlias = list[Game]
+"""A record of multiple games"""
 
 
 def solution1(puzzle_input) -> int:
