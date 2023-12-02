a9a07b0e6e572f3ef732c0318f0b3953cd66862a
Jb DOYON <jb@jiby.tech>
2023-12-02 15:26:43 +0000

    Add day2 sample inputs

--- src/advent_of_code/aoc2023/day2.py
+++ src/advent_of_code/aoc2023/day2.py
@@ -14,6 +14,41 @@ Game: TypeAlias = tuple[GameID, list[Grab]]
 Games: TypeAlias = list[Game]
 """A record of multiple games"""
 
+SAMPLE_INPUT_STR = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+SAMPLE_INPUT: Games = [
+    (1, [{"blue": 3, "red": 4}, {"red": 1, "green": 2, "blue": 6}, {"green": 2}]),
+    (
+        2,
+        [
+            {"blue": 1, "green": 2},
+            {"green": 3, "blue": 4, "red": 1},
+            {"green": 1, "blue": 1},
+        ],
+    ),
+    (
+        3,
+        [
+            {"green": 8, "blue": 6, "red": 20},
+            {"blue": 5, "red": 4, "green": 13},
+            {"green": 5, "red": 1},
+        ],
+    ),
+    (
+        4,
+        [
+            {"green": 1, "red": 3, "blue": 6},
+            {"green": 3, "red": 6},
+            {"green": 3, "blue": 15, "red": 14},
+        ],
+    ),
+    (5, [{"red": 6, "blue": 1, "green": 3}, {"blue": 2, "red": 1, "green": 2}]),
+]
+
 
 def solution1(puzzle_input) -> int:
     """Solve day2 part 1"""
