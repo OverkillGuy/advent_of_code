7f831ac35f13499641428852d7b28d500a0bd8af
Jb DOYON <jb@jiby.tech>
2024-12-01 14:32:46 +0000

    Add 2024 day 1 stub

---
+++ src/advent_of_code/aoc2024/__init__.py
@@ -0,0 +1,12 @@
+"""Per day solutions file of Advent Of Code 2024."""
+
+from typing import Callable
+
+from advent_of_code import Day, Part
+from advent_of_code.aoc2024 import day1
+
+SOLUTIONS_2024: dict[Day, dict[Part, Callable]] = {
+    1: {
+        1: day1.solve1_string,
+    },
+}
