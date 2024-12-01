db2234c4e57145364fd27ddfa96b2cc69490bae9
Jb DOYON <jb@jiby.tech>
2024-12-01 15:04:49 +0000

    Add input parsing for 2024 day1

--- src/advent_of_code/aoc2024/day1.py
+++ src/advent_of_code/aoc2024/day1.py
@@ -1,19 +1,36 @@
 """Day 1 solution to AoC 2024"""
 
+SAMPLE_INPUT = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
 
-def solution1(puzzle_input) -> int:
+Location = int
+LocationList = list[Location]
+PuzzleInput = tuple[LocationList, LocationList]
+
+
+def solution1(puzzle_input: PuzzleInput) -> int:
     """Solve day1 part 1"""
     return 0
 
 
-def solution2(puzzle_input) -> int:
+def solution2(puzzle_input: PuzzleInput) -> int:
     """Solve day1 part 2"""
     return 0
 
 
-def read_puzzle_input(puzzle_input: str):
-    """Process the puzzle input string"""
-    return puzzle_input
+def read_puzzle_input(puzzle_input: str) -> PuzzleInput:
+    """Process the puzzle input string
+
+    >>> read_puzzle_input(SAMPLE_INPUT)
+    ([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3])
+    """
+    lines = puzzle_input.splitlines()
+    grid = [line.split("   ") for line in lines]
+    return [int(left) for left, right in grid], [int(right) for left, right in grid]
 
 
 def solve1_string(puzzle_input: str) -> int:
